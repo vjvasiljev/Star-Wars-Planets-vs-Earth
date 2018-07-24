82388b7055100f1a8fbe941660775bad8ebd0866
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-24 22:53:09 +0300

    add Graph components for all int values

--- src/Graph.js
+++ src/Graph.js
@@ -2,15 +2,15 @@ import React from 'react';
 import 'tachyons';
 
 const Graph = (props) => {
-  console.log("Graph", props.proportions);
+  console.log("Graph", props);
   const mainClass = "h-5 ma0 pa2 dib";
   let greenClass = `${mainClass} bg-green`;
   let redClass = `${mainClass} bg-red`;
   console.log("greenClass", greenClass);
   return (
-    <div>
-      <div className={greenClass} style={{ width: (props.proportions.population) * 3 }}>{props.proportions.population}</div>
-      <div className={redClass} style={{ width: (100-props.proportions.population)*3}}>{100-props.proportions.population}</div>
+    <div className ="ma1">
+      <div className={greenClass} style={{ width: (props.proportions) * 3 }}>{props.proportions}</div>
+      <div className={redClass} style={{ width: (100-props.proportions)*3}}>{100-props.proportions}</div>
     </div>
   )
 }
